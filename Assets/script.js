bd3a70d7f7e0d113d4cd7a8cfdac811aaae2f33b
Alka sah <alkasah485@gmail.com>
2022-06-09 23:19:35 -0700

    added comment

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,33 @@
+//What sort of data does my application need to work
+
+// storing all needed chracters in variables.use array
+
+// numbers array
+
+//var numbers = "0123456789".split("");
+
+//special chracters  array
+
+// lowercase letters array
+
+//uppercase letters array
+
+// What sorts of task does my code need to complete
+
+// Generate Password
+
+
 // Assignment Code
-//generate password
+
+
 function generatePassword(){
-  // Prompt the User for their password length and store it in variable
-  //valiadte the paasword length and make sure it
+  // Prompt the User for their 'password length' and store it in variable
+  //validate the paasword length and make sure it's number between
+  //or equal to 8-128
+  //if password length greaterthan equal to 8 and less than or equal to 128
+  //randomly select chracter
+  //validate that atleast one chracter choice is true
+
 }
 var generateBtn = document.querySelector("#generate");
 
