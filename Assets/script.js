94ecd3324d23da45112e2bfc2dc78cbf0d533629
Alka sah <alkasah485@gmail.com>
2022-06-13 09:38:55 -0700

    declared array

--- Assets/script.js
+++ Assets/script.js
@@ -1,30 +1,18 @@
-//What sort of data does my application need to work
-
-// storing all needed chracters in variables.use array
-
-// numbers array
-
-//var numbers = "0123456789".split("");
-
-//special chracters  array
-
-// lowercase letters array
-
-//uppercase letters array
-
-// What sorts of task does my code need to complete
+// storing all needed chracters in variables
+var dataUppercase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+var dataLowercase = "abcdefghijklmnopqrstuvwxyz".split('');
+var dataNumbers   = "0123456789".split("");
+var dataSymbols   = "`~!@#$%^&*()-_=+[]{}|':;?/.,".split('');
 
 // Generate Password
 
-
-// Assignment Code
-
-
 function generatePassword(){
-  // Prompt the User for their 'password length' and store it in variable
+  // Prompt the User for their 'password length' and
+  // store it in variable
   //validate the paasword length and make sure it's number between
   //or equal to 8-128
-  //if password length greaterthan equal to 8 and less than or equal to 128
+  //if password length greaterthan equal to 8 and 
+  //less than or equal to 128
   //randomly select chracter
   //validate that atleast one chracter choice is true
 
