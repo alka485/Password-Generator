d0093c6db19075e91d69506a086f4a73db05ae08
Alka sah <alkasah485@gmail.com>
2022-06-15 06:22:56 -0700

    formatting the javascript code

--- Assets/script.js
+++ Assets/script.js
@@ -8,45 +8,30 @@ var dataSymbols = "`~!@#$%^&*()-_=+[]{}|':;?/.,".split('');
 
 function generatePassword() {
   var userChoiceChracters = prompt("How many chracters would you like in our password?");
-  console.log(userChoiceChracters);
 
   if (userChoiceChracters >= 8 && userChoiceChracters <= 128) {
     userChoiceSymbols = confirm("Do you want to include symbols?");
-    console.log(userChoiceSymbols);
   } else {
     alert("password must atleast contain 8 to 128 chracters");
   }
-
   if (userChoiceSymbols == true || userChoiceSymbols == false) {
-    var userChoiceNumbers = confirm("DO you want to include numbers?");
-    console.log(userChoiceNumbers);
-
+    var userChoiceNumbers = confirm("Do you want to include numbers?");
     if (userChoiceNumbers == true || userChoiceNumbers == false) {
       var userChoiceUpper = confirm("Do you want to include upper case letter?");
-      console.log(userChoiceUpper);
-
       if (userChoiceUpper == true || userChoiceUpper == false) {
         var userChoiceLower = confirm("Do you want to include lower case ?");
-        console.log(userChoiceLower);
       }
-
       if (userChoiceSymbols == false && userChoiceNumbers == false && userChoiceLower == false && userChoiceUpper == false) {
         alert("please select atleast one box !");
       }
-      
     }
   }
-
-
   var data = ''.concat(dataLowercase, dataNumbers, dataSymbols, dataUppercase);
-  console.log(data.length);
   var passwordLength = userChoiceChracters;
-  console.log(passwordLength);
   var newPassword = '';
-
+  //for loop
   for (var i = 0; i < passwordLength; i++) {
     newPassword += data[Math.floor(Math.random() * data.length)];
-    console.log(newPassword);
   }
   return newPassword;
 
